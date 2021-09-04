f89ae021935df170d29d481ba8486e5b829dcd23
Lassi Kortela <lassi@lassi.io>
2021-09-04 15:23:35 +0300

    Add cleaning script

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eux
+rm -f pandoc.build.sh
+rm -f pandoc.import.scm
+rm -f pandoc.import.so
+rm -f pandoc.install.sh
+rm -f pandoc.link
+rm -f pandoc.server.import.scm
+rm -f pandoc.server.import.so
+rm -f pandoc.server.link
+rm -f pandoc.server.so
+rm -f pandoc.server.static.o
+rm -f pandoc.so
+rm -f pandoc.static.o
+rm -f pandoc.tar.import.scm
+rm -f pandoc.tar.link
+rm -f pandoc.tar.static.o
